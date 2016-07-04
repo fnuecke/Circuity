10f89c67c65a6d0b5a98015429804795c59643f4
Florian Nücke <florian@nuecke.de>
2016-07-04 19:15:08 +0200

    Unit tests for serialization and synchronization libraries.

---
+++ src/test/java/li/cil/lib/SerializationTest.java
@@ -0,0 +1,329 @@
+package li.cil.lib;
+
+import com.google.common.util.concurrent.Futures;
+import li.cil.lib.api.SillyBeeAPI;
+import li.cil.lib.api.serialization.Serializable;
+import li.cil.lib.api.serialization.Serialize;
+import li.cil.lib.api.serialization.SerializerCollection;
+import net.minecraftforge.fml.relauncher.Side;
+import org.junit.Test;
+
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import static org.junit.Assert.*;
+
+public class SerializationTest {
+    private final SerializerCollection s;
+
+    public SerializationTest() {
+        Serialization.init();
+        s = SillyBeeAPI.serialization.get(Side.SERVER);
+    }
+
+    @Test
+    public void getSerializers() throws Exception {
+        assertNotNull(SillyBeeAPI.serialization.get(Side.CLIENT));
+        assertNotNull(SillyBeeAPI.serialization.get(Side.SERVER));
+
+        assertNotEquals(SillyBeeAPI.serialization.get(Side.CLIENT), SillyBeeAPI.serialization.get(Side.SERVER));
+    }
+
+    @Test
+    public void serializeByte() {
+        testRoundabout(s, Byte.MIN_VALUE, byte.class);
+        testRoundabout(s, Byte.MAX_VALUE, byte.class);
+        testRoundabout(s, (byte) -1, byte.class);
+        testRoundabout(s, (byte) -0, byte.class);
+        testRoundabout(s, (byte) 0, byte.class);
+        testRoundabout(s, (byte) 1, byte.class);
+        testRoundabout(s, (byte) -123, byte.class);
+        testRoundabout(s, (byte) 123, byte.class);
+    }
+
+    @Test
+    public void serializeShort() {
+        testRoundabout(s, Short.MIN_VALUE, short.class);
+        testRoundabout(s, Short.MAX_VALUE, short.class);
+        testRoundabout(s, (short) -1, short.class);
+        testRoundabout(s, (short) -0, short.class);
+        testRoundabout(s, (short) 0, short.class);
+        testRoundabout(s, (short) 1, short.class);
+        testRoundabout(s, (short) -123, short.class);
+        testRoundabout(s, (short) 123, short.class);
+    }
+
+    @Test
+    public void serializeInt() {
+        testRoundabout(s, Integer.MIN_VALUE, int.class);
+        testRoundabout(s, Integer.MAX_VALUE, int.class);
+        testRoundabout(s, -1, int.class);
+        testRoundabout(s, -0, int.class);
+        testRoundabout(s, 0, int.class);
+        testRoundabout(s, 1, int.class);
+        testRoundabout(s, -123, int.class);
+        testRoundabout(s, 123, int.class);
+    }
+
+    @Test
+    public void serializeLong() {
+        testRoundabout(s, Long.MIN_VALUE, long.class);
+        testRoundabout(s, Long.MAX_VALUE, long.class);
+        testRoundabout(s, -1L, long.class);
+        testRoundabout(s, -0L, long.class);
+        testRoundabout(s, 0L, long.class);
+        testRoundabout(s, 1L, long.class);
+        testRoundabout(s, -123L, long.class);
+        testRoundabout(s, 123L, long.class);
+    }
+
+    @Test
+    public void serializeFloat() {
+        testRoundabout(s, Float.MIN_VALUE, float.class);
+        testRoundabout(s, Float.MAX_VALUE, float.class);
+        testRoundabout(s, Float.NEGATIVE_INFINITY, float.class);
+        testRoundabout(s, Float.POSITIVE_INFINITY, float.class);
+        testRoundabout(s, Float.NaN, float.class);
+        testRoundabout(s, -1.0f, float.class);
+        testRoundabout(s, -0.0f, float.class);
+        testRoundabout(s, 0.0f, float.class);
+        testRoundabout(s, 1.0f, float.class);
+        testRoundabout(s, -123.45f, float.class);
+        testRoundabout(s, 123.45f, float.class);
+    }
+
+    @Test
+    public void serializeDouble() {
+        testRoundabout(s, Double.MIN_VALUE, double.class);
+        testRoundabout(s, Double.MAX_VALUE, double.class);
+        testRoundabout(s, Double.NEGATIVE_INFINITY, double.class);
+        testRoundabout(s, Double.POSITIVE_INFINITY, double.class);
+        testRoundabout(s, Double.NaN, double.class);
+        testRoundabout(s, -1.0, double.class);
+        testRoundabout(s, -0.0, double.class);
+        testRoundabout(s, 0.0, double.class);
+        testRoundabout(s, 1.0, double.class);
+        testRoundabout(s, -123.45, double.class);
+        testRoundabout(s, 123.45, double.class);
+    }
+
+    @Test
+    public void serializeBoolean() {
+        testRoundabout(s, false, boolean.class);
+        testRoundabout(s, true, boolean.class);
+    }
+
+    @Test
+    public void serializeChar() {
+        testRoundabout(s, Character.MIN_VALUE, char.class);
+        testRoundabout(s, Character.MAX_VALUE, char.class);
+        testRoundabout(s, (char) -1, char.class);
+        testRoundabout(s, (char) -0, char.class);
+        testRoundabout(s, (char) 0, char.class);
+        testRoundabout(s, (char) 1, char.class);
+        testRoundabout(s, (char) -123, char.class);
+        testRoundabout(s, (char) 123, char.class);
+        testRoundabout(s, 'a', char.class);
+        testRoundabout(s, 'A', char.class);
+        testRoundabout(s, 'z', char.class);
+        testRoundabout(s, 'Z', char.class);
+        testRoundabout(s, '\uBEEF', char.class);
+    }
+
+    @Test
+    public void serializeString() {
+        testRoundabout(s, "", String.class);
+        testRoundabout(s, "foo bar baz", String.class);
+        testRoundabout(s, "foo \0 bar", String.class);
+        testRoundabout(s, "foo \uBEEF bar", String.class);
+    }
+
+    @Test
+    public void serializeByteArray() {
+        {
+            final byte[] data1 = new byte[]{};
+            final byte[] data2 = s.deserialize(byte[].class, s.serialize(data1));
+            assertArrayEquals(data1, data2);
+        }
+
+        {
+            final byte[] data1 = new byte[]{1, 2, 3, 4};
+            final byte[] data2 = s.deserialize(byte[].class, s.serialize(data1));
+            assertArrayEquals(data1, data2);
+        }
+    }
+
+    @Test
+    public void serializeIntArray() {
+        {
+            final int[] data1 = new int[]{};
+            final int[] data2 = s.deserialize(int[].class, s.serialize(data1));
+            assertArrayEquals(data1, data2);
+        }
+        {
+            final int[] data1 = new int[]{1, 2, 3, 4};
+            final int[] data2 = s.deserialize(int[].class, s.serialize(data1));
+            assertArrayEquals(data1, data2);
+        }
+    }
+
+    @Test
+    public void serializeEnum() {
+        testRoundabout(s, TestEnum.ONE, TestEnum.class);
+        testRoundabout(s, TestEnum.TWO, TestEnum.class);
+        testRoundabout(s, TestEnum.THREE, TestEnum.class);
+    }
+
+    @Test
+    public void serializeClass() {
+        testRoundabout(s, Object.class, Class.class);
+        testRoundabout(s, String.class, Class.class);
+        testRoundabout(s, int.class, Class.class);
+    }
+
+    @Test
+    public void serializeFuture() {
+        {
+            final Future<String> data1 = Futures.immediateFuture("foo bar");
+            final Future data2 = s.deserialize(Future.class, s.serialize(data1));
+            try {
+                assertEquals(data1.get(), data2.get());
+            } catch (InterruptedException | ExecutionException e) {
+                fail("wat");
+            }
+        }
+    }
+
+    @Test
+    public void serializeObjectArray() {
+        {
+            final String[] data1 = new String[]{};
+            final String[] data2 = s.deserialize(String[].class, s.serialize(data1));
+            assertArrayEquals(data1, data2);
+        }
+        {
+            final String[] data1 = new String[]{"foo", "bar", "baz"};
+            final String[] data2 = s.deserialize(String[].class, s.serialize(data1));
+            assertArrayEquals(data1, data2);
+        }
+    }
+
+    @Test
+    public void serializeObjectArrayInto() {
+        {
+            final String[] data1 = new String[]{"foo", "bar", "baz"};
+            final String[] data2 = new String[]{"lol", "wat", "hey"};
+            final String[] data3 = s.deserialize(data2, s.serialize(data1));
+            assertArrayEquals(data1, data3);
+            assertArrayEquals(data2, data3); // Same length, reused.
+        }
+    }
+
+    @Test
+    public void serializeCollection() {
+        {
+            final List<String> data1 = new ArrayList<>();
+            final List<String> data2 = s.deserialize(ArrayList.class, s.serialize(data1));
+            assertArrayEquals(data1.toArray(), data2.toArray());
+        }
+        {
+            final List<String> data1 = Arrays.asList("foo", "bar", "baz");
+            final List<String> data2 = s.deserialize(ArrayList.class, s.serialize(data1));
+            assertArrayEquals(data1.toArray(), data2.toArray());
+        }
+
+        {
+            final Set<String> data1 = new HashSet<>();
+            final Set<String> data2 = s.deserialize(HashSet.class, s.serialize(data1));
+            assertArrayEquals(data1.toArray(), data2.toArray());
+        }
+        {
+            final Set<String> data1 = new HashSet<>(Arrays.asList("foo", "bar", "baz"));
+            final Set<String> data2 = s.deserialize(HashSet.class, s.serialize(data1));
+            assertArrayEquals(data1.toArray(), data2.toArray());
+        }
+    }
+
+    @Test
+    public void serializeSerializable() {
+        {
+            final A a1 = new A();
+            a1.b = false;
+            a1.increment();
+            final A a2 = s.deserialize(A.class, s.serialize(a1));
+
+            assertEquals(a1, a2);
+        }
+
+        {
+            final A a1 = new A();
+            a1.b = false;
+            a1.increment();
+
+            final A a2 = new A();
+            a2.increment();
+            a2.increment();
+
+            final B b1 = new B();
+            b1.a = a1;
+            b1.is = new int[]{3, 4, 5};
+            b1.as.add(a1);
+            b1.as.add(a2);
+
+            final B b2 = s.deserialize(B.class, s.serialize(b1));
+
+            assertEquals(b1.a, b2.a);
+            assertArrayEquals(b1.is, b2.is);
+            assertArrayEquals(b1.as.toArray(), b2.as.toArray());
+        }
+    }
+
+    public enum TestEnum {
+        ONE,
+        TWO,
+        THREE
+    }
+
+    @Serializable
+    public static class A {
+        @Serialize
+        public boolean b = true;
+
+        @Serialize
+        private int i = 10;
+
+        public void increment() {
+            i++;
+        }
+
+        @Override
+        public boolean equals(@Nullable final Object obj) {
+            if (obj == this) return true;
+            if (!(obj instanceof A)) return false;
+            final A other = (A) obj;
+            return other.b == b && other.i == i;
+        }
+    }
+
+    @Serializable
+    private static final class B {
+        @Serialize
+        public A a;
+
+        @Serialize
+        public int[] is = new int[]{1, 2, 3};
+
+        @Serialize
+        public List<A> as = new ArrayList<>();
+    }
+
+    private static <T> void testRoundabout(final SerializerCollection s, final T data, final Class<T> clazz) {
+        assertEquals(data, s.deserialize(clazz, s.serialize(data)));
+    }
+}
---
+++ src/test/java/li/cil/lib/SynchronizationTest.java
@@ -0,0 +1,462 @@
+package li.cil.lib;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import li.cil.lib.api.serialization.Serializable;
+import li.cil.lib.api.serialization.Serialize;
+import li.cil.lib.api.synchronization.SynchronizationManagerServer;
+import li.cil.lib.synchronization.value.SynchronizedArray;
+import li.cil.lib.synchronization.value.SynchronizedByte;
+import li.cil.lib.synchronization.value.SynchronizedByteArray;
+import li.cil.lib.synchronization.value.SynchronizedChar;
+import li.cil.lib.synchronization.value.SynchronizedDouble;
+import li.cil.lib.synchronization.value.SynchronizedFloat;
+import li.cil.lib.synchronization.value.SynchronizedInt;
+import li.cil.lib.synchronization.value.SynchronizedIntArray;
+import li.cil.lib.synchronization.value.SynchronizedLong;
+import li.cil.lib.synchronization.value.SynchronizedObject;
+import li.cil.lib.synchronization.value.SynchronizedShort;
+import li.cil.lib.synchronization.value.SynchronizedString;
+import net.minecraft.network.PacketBuffer;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.Collections;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+public class SynchronizationTest {
+    @Test
+    public void synchronizeStrings() {
+        synchronizeString("");
+        synchronizeString("foo bar baz");
+        synchronizeString("foo \uBEEF bar");
+    }
+
+    @Test
+    public void synchronizeDoubles() {
+        synchronizeDouble(Double.MIN_VALUE);
+        synchronizeDouble(Double.MAX_VALUE);
+        synchronizeDouble(-123.45);
+        synchronizeDouble(-1.0);
+        synchronizeDouble(-0.0);
+        synchronizeDouble(0.0);
+        synchronizeDouble(1.0);
+        synchronizeDouble(123.45);
+    }
+
+    @Test
+    public void synchronizeFloats() {
+        synchronizeFloat(Float.MIN_VALUE);
+        synchronizeFloat(Float.MAX_VALUE);
+        synchronizeFloat(-123.45f);
+        synchronizeFloat(-1.0f);
+        synchronizeFloat(-0.0f);
+        synchronizeFloat(0.0f);
+        synchronizeFloat(1.0f);
+        synchronizeFloat(123.45f);
+    }
+
+    @Test
+    public void synchronizeChars() {
+        synchronizeChar(Character.MIN_VALUE);
+        synchronizeChar(Character.MAX_VALUE);
+        synchronizeChar((char) -1);
+        synchronizeChar((char) -0);
+        synchronizeChar((char) 0);
+        synchronizeChar((char) 1);
+        synchronizeChar('a');
+        synchronizeChar('A');
+        synchronizeChar('z');
+        synchronizeChar('Z');
+        synchronizeChar('\uBEEF');
+    }
+
+    @Test
+    public void synchronizeLongs() {
+        synchronizeLong(Long.MIN_VALUE);
+        synchronizeLong(Long.MAX_VALUE);
+        synchronizeLong(-1L);
+        synchronizeLong(-0L);
+        synchronizeLong(0L);
+        synchronizeLong(1L);
+    }
+
+    @Test
+    public void synchronizeInts() {
+        synchronizeInt(Integer.MIN_VALUE);
+        synchronizeInt(Integer.MAX_VALUE);
+        synchronizeInt(-1);
+        synchronizeInt(-0);
+        synchronizeInt(0);
+        synchronizeInt(1);
+    }
+
+    @Test
+    public void synchronizeShorts() {
+        synchronizeShort(Short.MIN_VALUE);
+        synchronizeShort(Short.MAX_VALUE);
+        synchronizeShort((short) -1);
+        synchronizeShort((short) -0);
+        synchronizeShort((short) 0);
+        synchronizeShort((short) 1);
+    }
+
+    @Test
+    public void synchronizeBytes() {
+        synchronizeByte(Byte.MIN_VALUE);
+        synchronizeByte(Byte.MAX_VALUE);
+        synchronizeByte((byte) -1);
+        synchronizeByte((byte) -0);
+        synchronizeByte((byte) 0);
+        synchronizeByte((byte) 1);
+    }
+
+    @Test
+    public void synchronizeByteArrays() {
+        synchronizeByteArray(new byte[]{1, 2, 3, 4}, (byte) 6);
+        synchronizeByteArray(new byte[]{5, 6, 7, 8}, (byte) 9);
+    }
+
+    @Test
+    public void synchronizeIntArrays() {
+        synchronizeIntArray(new int[]{1, 2, 3, 4}, 6);
+        synchronizeIntArray(new int[]{5, 6, 7, 8}, 9);
+    }
+
+    @Test
+    public void synchronizeArrays() {
+        synchronizeArray(Integer.class, new Integer[]{1, 2, 3, 4}, 6);
+        synchronizeArray(Double.class, new Double[]{1.0, 2.0, 3.0, 4.0}, 6.0);
+        synchronizeArray(String.class, new String[]{"foo", "bar", "baz", "123"}, "bcx");
+    }
+
+    @Test
+    public void synchronizeObjects() {
+        synchronizeObject(String.class, "foo bar baz");
+        synchronizeObject(A.class, new A(5.432f));
+    }
+
+    @Serializable
+    public static final class A {
+        @Serialize
+        public float f = 1.234f;
+
+        public A() {
+        }
+
+        public A(final float f) {
+            this.f = f;
+        }
+
+        @Override
+        public boolean equals(final Object obj) {
+            if (obj == this) return true;
+            if (!(obj instanceof A)) return false;
+            final A other = (A) obj;
+            return other.f == f;
+        }
+    }
+
+    private static void synchronizeByte(final byte value) {
+        final SynchronizationManagerServer manager = Mockito.mock(SynchronizationManagerServer.class);
+
+        final SynchronizedByte i1 = new SynchronizedByte((byte) 42);
+        i1.setManager(manager);
+        i1.set(value);
+
+        assertEquals(i1.get(), value);
+
+        verify(manager, times(1)).setDirty(i1, null);
+
+        final ByteBuf buffer = Unpooled.buffer();
+        final PacketBuffer packet = new PacketBuffer(buffer);
+        i1.serialize(packet, null);
+
+        final SynchronizedByte i2 = new SynchronizedByte((byte) 42);
+
+        assertNotEquals(i2.get(), value);
+
+        i2.deserialize(packet);
+
+        assertEquals(i2.get(), value);
+    }
+
+    private static void synchronizeShort(final short value) {
+        final SynchronizationManagerServer manager = Mockito.mock(SynchronizationManagerServer.class);
+
+        final SynchronizedShort i1 = new SynchronizedShort((short) 42);
+        i1.setManager(manager);
+        i1.set(value);
+
+        assertEquals(i1.get(), value);
+
+        verify(manager, times(1)).setDirty(i1, null);
+
+        final ByteBuf buffer = Unpooled.buffer();
+        final PacketBuffer packet = new PacketBuffer(buffer);
+        i1.serialize(packet, null);
+
+        final SynchronizedShort i2 = new SynchronizedShort((short) 42);
+
+        assertNotEquals(i2.get(), value);
+
+        i2.deserialize(packet);
+
+        assertEquals(i2.get(), value);
+    }
+
+    private static void synchronizeInt(final int value) {
+        final SynchronizationManagerServer manager = Mockito.mock(SynchronizationManagerServer.class);
+
+        final SynchronizedInt i1 = new SynchronizedInt(42);
+        i1.setManager(manager);
+        i1.set(value);
+
+        assertEquals(i1.get(), value);
+
+        verify(manager, times(1)).setDirty(i1, null);
+
+        final ByteBuf buffer = Unpooled.buffer();
+        final PacketBuffer packet = new PacketBuffer(buffer);
+        i1.serialize(packet, null);
+
+        final SynchronizedInt i2 = new SynchronizedInt(42);
+
+        assertNotEquals(i2.get(), value);
+
+        i2.deserialize(packet);
+
+        assertEquals(i2.get(), value);
+    }
+
+    private static void synchronizeLong(final long value) {
+        final SynchronizationManagerServer manager = Mockito.mock(SynchronizationManagerServer.class);
+
+        final SynchronizedLong i1 = new SynchronizedLong(42);
+        i1.setManager(manager);
+        i1.set(value);
+
+        assertEquals(i1.get(), value);
+
+        verify(manager, times(1)).setDirty(i1, null);
+
+        final ByteBuf buffer = Unpooled.buffer();
+        final PacketBuffer packet = new PacketBuffer(buffer);
+        i1.serialize(packet, null);
+
+        final SynchronizedLong i2 = new SynchronizedLong(42);
+
+        assertNotEquals(i2.get(), value);
+
+        i2.deserialize(packet);
+
+        assertEquals(i2.get(), value);
+    }
+
+    private static void synchronizeChar(final char value) {
+        final SynchronizationManagerServer manager = Mockito.mock(SynchronizationManagerServer.class);
+
+        final SynchronizedChar i1 = new SynchronizedChar((char) 42);
+        i1.setManager(manager);
+        i1.set(value);
+
+        assertEquals(i1.get(), value);
+
+        verify(manager, times(1)).setDirty(i1, null);
+
+        final ByteBuf buffer = Unpooled.buffer();
+        final PacketBuffer packet = new PacketBuffer(buffer);
+        i1.serialize(packet, null);
+
+        final SynchronizedChar i2 = new SynchronizedChar((char) 42);
+
+        assertNotEquals(i2.get(), value);
+
+        i2.deserialize(packet);
+
+        assertEquals(i2.get(), value);
+    }
+
+    private static void synchronizeFloat(final float value) {
+        final SynchronizationManagerServer manager = Mockito.mock(SynchronizationManagerServer.class);
+
+        final SynchronizedFloat i1 = new SynchronizedFloat(42);
+        i1.setManager(manager);
+        i1.set(value);
+
+        assertEquals(i1.get(), value, 0.0f);
+
+        verify(manager, times(1)).setDirty(i1, null);
+
+        final ByteBuf buffer = Unpooled.buffer();
+        final PacketBuffer packet = new PacketBuffer(buffer);
+        i1.serialize(packet, null);
+
+        final SynchronizedFloat i2 = new SynchronizedFloat(42);
+
+        assertNotEquals(i2.get(), value);
+
+        i2.deserialize(packet);
+
+        assertEquals(i2.get(), value, 0.0f);
+    }
+
+    private static void synchronizeDouble(final double value) {
+        final SynchronizationManagerServer manager = Mockito.mock(SynchronizationManagerServer.class);
+
+        final SynchronizedDouble i1 = new SynchronizedDouble(42);
+        i1.setManager(manager);
+        i1.set(value);
+
+        assertEquals(i1.get(), value, 0.0);
+
+        verify(manager, times(1)).setDirty(i1, null);
+
+        final ByteBuf buffer = Unpooled.buffer();
+        final PacketBuffer packet = new PacketBuffer(buffer);
+        i1.serialize(packet, null);
+
+        final SynchronizedDouble i2 = new SynchronizedDouble(42);
+
+        assertNotEquals(i2.get(), value);
+
+        i2.deserialize(packet);
+
+        assertEquals(i2.get(), value, 0.0);
+    }
+
+    private static void synchronizeString(final String value) {
+        final SynchronizationManagerServer manager = Mockito.mock(SynchronizationManagerServer.class);
+
+        final SynchronizedString i1 = new SynchronizedString("nope");
+        i1.setManager(manager);
+        i1.set(value);
+
+        assertEquals(i1.get(), value);
+
+        verify(manager, times(1)).setDirty(i1, null);
+
+        final ByteBuf buffer = Unpooled.buffer();
+        final PacketBuffer packet = new PacketBuffer(buffer);
+        i1.serialize(packet, null);
+
+        final SynchronizedString i2 = new SynchronizedString("nope");
+
+        assertNotEquals(i2.get(), value);
+
+        i2.deserialize(packet);
+
+        assertEquals(i2.get(), value);
+    }
+
+    private static <T> void synchronizeByteArray(final byte[] data, final byte value) {
+        final SynchronizationManagerServer manager = Mockito.mock(SynchronizationManagerServer.class);
+
+        final byte[] copy = data.clone();
+
+        final SynchronizedByteArray i1 = new SynchronizedByteArray(data);
+        i1.setManager(manager);
+        i1.set(2, value);
+
+        assertEquals(i1.get(2), value);
+
+        verify(manager, times(1)).setDirty(i1, 2);
+
+        final ByteBuf buffer = Unpooled.buffer();
+        final PacketBuffer packet = new PacketBuffer(buffer);
+        i1.serialize(packet, Collections.singletonList(2));
+
+        final SynchronizedByteArray i2 = new SynchronizedByteArray(data);
+
+        assertNotEquals(i2.get(2), value);
+
+        i2.deserialize(packet);
+
+        assertEquals(i2.get(2), value);
+
+        assertArrayEquals(data, copy);
+    }
+
+    private static <T> void synchronizeIntArray(final int[] data, final int value) {
+        final SynchronizationManagerServer manager = Mockito.mock(SynchronizationManagerServer.class);
+
+        final int[] copy = data.clone();
+
+        final SynchronizedIntArray i1 = new SynchronizedIntArray(data);
+        i1.setManager(manager);
+        i1.set(2, value);
+
+        assertEquals(i1.get(2), value);
+
+        verify(manager, times(1)).setDirty(i1, 2);
+
+        final ByteBuf buffer = Unpooled.buffer();
+        final PacketBuffer packet = new PacketBuffer(buffer);
+        i1.serialize(packet, Collections.singletonList(2));
+
+        final SynchronizedIntArray i2 = new SynchronizedIntArray(data);
+
+        assertNotEquals(i2.get(2), value);
+
+        i2.deserialize(packet);
+
+        assertEquals(i2.get(2), value);
+
+        assertArrayEquals(data, copy);
+    }
+
+    private static <T> void synchronizeArray(final Class<T> elementType, final T[] data, final T value) {
+        final SynchronizationManagerServer manager = Mockito.mock(SynchronizationManagerServer.class);
+
+        final T[] copy = data.clone();
+
+        final SynchronizedArray<T> i1 = new SynchronizedArray<>(elementType, data);
+        i1.setManager(manager);
+        i1.set(2, value);
+
+        assertEquals(i1.get(2), value);
+
+        verify(manager, times(1)).setDirty(i1, 2);
+
+        final ByteBuf buffer = Unpooled.buffer();
+        final PacketBuffer packet = new PacketBuffer(buffer);
+        i1.serialize(packet, Collections.singletonList(2));
+
+        final SynchronizedArray<T> i2 = new SynchronizedArray<>(elementType, data);
+
+        assertNotEquals(i2.get(2), value);
+
+        i2.deserialize(packet);
+
+        assertEquals(i2.get(2), value);
+
+        assertArrayEquals(data, copy);
+    }
+
+    private static <T> void synchronizeObject(final Class<T> clazz, final T value) {
+        final SynchronizationManagerServer manager = Mockito.mock(SynchronizationManagerServer.class);
+
+        final SynchronizedObject<T> i1 = new SynchronizedObject<>(clazz);
+        i1.setManager(manager);
+        i1.set(value);
+
+        assertEquals(i1.get(), value);
+
+        verify(manager, times(1)).setDirty(i1, null);
+
+        final ByteBuf buffer = Unpooled.buffer();
+        final PacketBuffer packet = new PacketBuffer(buffer);
+        i1.serialize(packet, null);
+
+        final SynchronizedObject<T> i2 = new SynchronizedObject<>(clazz);
+
+        assertNotEquals(i2.get(), value);
+
+        i2.deserialize(packet);
+
+        assertEquals(i2.get(), value);
+    }
+}
