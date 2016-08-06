1dbeaaf9208131f56d12e0d8e36e75f31ecef0cf
Florian Nücke <florian@nuecke.de>
2016-08-06 02:58:08 +0200

    Mustn't return null from capability storage to avoid item stacks being saved erroring...

--- src/main/java/li/cil/circuity/common/capabilities/eeprom/EEPROMStorage.java
+++ src/main/java/li/cil/circuity/common/capabilities/eeprom/EEPROMStorage.java
@@ -20,30 +20,34 @@ public final class EEPROMStorage implements Capability.IStorage<EEPROM> {
     @Override
     @Nullable
     public NBTBase writeNBT(final Capability<EEPROM> capability, final EEPROM instance, final EnumFacing side) {
+        final NBTTagCompound compound = new NBTTagCompound();
+
         if (instance instanceof EEPROMImpl) {
             final EEPROMImpl eeprom = (EEPROMImpl) instance;
             if (eeprom.uuid != null) {
-                final NBTTagCompound compound = new NBTTagCompound();
                 compound.setLong(MOST_SIGNIFICANT_BITS_TAG, eeprom.uuid.getMostSignificantBits());
                 compound.setLong(LEAST_SIGNIFICANT_BITS_TAG, eeprom.uuid.getLeastSignificantBits());
-                return compound;
             }
         } else {
             ModSillyBee.getLogger().error("Trying to save a non-default implementation of EEPROM using the default EEPROM storage implementation.");
         }
 
-        return null;
+        return compound;
     }
 
     @Override
     public void readNBT(final Capability<EEPROM> capability, final EEPROM instance, final EnumFacing side, final NBTBase nbt) {
         if (instance instanceof EEPROMImpl) {
+            final EEPROMImpl eeprom = (EEPROMImpl) instance;
             if (nbt instanceof NBTTagCompound) {
-                final EEPROMImpl eeprom = (EEPROMImpl) instance;
                 final NBTTagCompound compound = (NBTTagCompound) nbt;
-                final long mostSignificantBits = compound.getLong(MOST_SIGNIFICANT_BITS_TAG);
-                final long leastSignificantBits = compound.getLong(LEAST_SIGNIFICANT_BITS_TAG);
-                eeprom.uuid = new UUID(mostSignificantBits, leastSignificantBits);
+                if (compound.hasKey(MOST_SIGNIFICANT_BITS_TAG) && compound.hasKey(LEAST_SIGNIFICANT_BITS_TAG)) {
+                    final long mostSignificantBits = compound.getLong(MOST_SIGNIFICANT_BITS_TAG);
+                    final long leastSignificantBits = compound.getLong(LEAST_SIGNIFICANT_BITS_TAG);
+                    eeprom.uuid = new UUID(mostSignificantBits, leastSignificantBits);
+                }
+            } else {
+                ModSillyBee.getLogger().error("Trying to load EEPROM from incompatible tag type.");
             }
         } else {
             ModSillyBee.getLogger().error("Trying to load a non-default implementation of EEPROM using the default EEPROM storage implementation.");
--- src/main/java/li/cil/lib/capabilities/NullStorage.java
+++ src/main/java/li/cil/lib/capabilities/NullStorage.java
@@ -1,16 +1,14 @@
 package li.cil.lib.capabilities;
 
 import net.minecraft.nbt.NBTBase;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumFacing;
 import net.minecraftforge.common.capabilities.Capability;
 
-import javax.annotation.Nullable;
-
 public final class NullStorage<T> implements Capability.IStorage<T> {
     @Override
-    @Nullable
     public NBTBase writeNBT(final Capability<T> capability, final T instance, final EnumFacing side) {
-        return null;
+        return new NBTTagCompound();
     }
 
     @Override
