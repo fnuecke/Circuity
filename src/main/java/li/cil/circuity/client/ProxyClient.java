a16001c793d07a024aef3d1a7e82b7bba1c81781
Florian Nücke <florian@nuecke.de>
2016-08-06 03:42:16 +0200

    Fixed item rendering, updated textures.

--- src/main/java/li/cil/circuity/client/ProxyClient.java
+++ src/main/java/li/cil/circuity/client/ProxyClient.java
@@ -1,6 +1,19 @@
 package li.cil.circuity.client;
 
 import li.cil.circuity.common.ProxyCommon;
+import li.cil.circuity.common.init.Items;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 public final class ProxyClient extends ProxyCommon {
+    @Override
+    public void preInit(final FMLPreInitializationEvent event) {
+        super.preInit(event);
+
+        for (final Item item : Items.all) {
+            ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(item.getRegistryName(), "inventory"));
+        }
+    }
 }
