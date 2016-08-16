b270797a12d96a416f1ec3ba086dec9a387a67f4
Florian Nücke <florian@nuecke.de>
2016-08-16 20:11:54 +0200

    Update testcase for CPU to use Z80 instruction set exerciser.

--- src/test/java/li/cil/circuity/server/processor/z80/Z80Test.java
+++ src/test/java/li/cil/circuity/server/processor/z80/Z80Test.java
@@ -18,11 +18,12 @@ public class Z80Test {
 
         final Z80 cpu = new Z80();
         cpu.setBusController(controller);
+        controller.setCPU(cpu);
 
         cpu.reset();
 
         final int diagnosticsOffset = 0x100;
-        final byte[] diagnosticsRom = Files.readAllBytes(Paths.get("src/test/resources/cpudiag.bin"));
+        final byte[] diagnosticsRom = Files.readAllBytes(Paths.get("src/test/resources/zexdoc.com"));
 
         cpu.setPC(diagnosticsOffset);
         for (int address = diagnosticsOffset, end = Math.min(0xFFFF, diagnosticsOffset + diagnosticsRom.length); address < end; ++address) {
@@ -31,29 +32,14 @@ public class Z80Test {
 
         final int biosOffset = 0x00;
         final byte[] biosRom = {
-                0x76,             // HLT       @ 0x00
-                0x00,             // NOP       @ 0x01
-                0x00,             // NOP       @ 0x02
-                0x00,             // NOP       @ 0x03
-                0x00,             // NOP       @ 0x04
-                0x3E, 0x02,       // LD A,2    @ 0x05
-         (byte) 0xB9,             // CMP C     @ 0x07
-         (byte) 0xCA, 0x12, 0x00, // JZ chout  @ 0x08
-                0x3E, 0x09,       // LD A,9    @ 0x0B
-         (byte) 0xB9,             // CMP C     @ 0x0D
-         (byte) 0xCA, 0x16, 0x00, // JZ sout   @ 0x0E
-         (byte) 0xC9,             // RET       @ 0x11
-                0x7B,             // LD A,E    @ 0x12 ; chout
-         (byte) 0xD3, 0x03,       // OUT A,(3) @ 0x13
-         (byte) 0xC9,             // RET       @ 0x15
-                0x06, 0x24,       // LD B,'$'  @ 0x16 ; sout
-                0x1A,             // LD A,(DE) @ 0x18 ; loop
-         (byte) 0xB8,             // CMP B     @ 0x19
-         (byte) 0xC8,             // RZ        @ 0x1A
-         (byte) 0xD3, 0x03,       // OUT A,(3) @ 0x1B
-                0x13,             // INC DE    @ 0x1D
-         (byte) 0xC3, 0x18, 0x00, // JP loop   @ 0x1E
-         (byte) 0xC9              // RET       @ 0x21
+         (byte) 0x76,       /* HLT */
+                0x00,
+                0x00,
+                0x00,
+                0x00,
+         (byte) 0xDB,       /* IN A, N */
+         (byte) 0x00,
+         (byte) 0xC9        /* RET */
         };
         for (int address = biosOffset, end = Math.min(0xFFFF, biosOffset + biosRom.length); address < end; ++address) {
             controller.mapAndWrite(address, biosRom[address - biosOffset] & 0xFF);
@@ -62,30 +48,39 @@ public class Z80Test {
         final int mhz = 2_000_000;
         final int tps = 20;
         final int cps = mhz / tps;
-        final int runFor = 10; // in seconds
-        for (int tick = 0, end = runFor * tps; tick < end; ++tick) {
-            cpu.run(cps);
+        while (cpu.run(cps)) {
         }
 
-        assertTrue(controller.serialConsole.toString().trim().equals("CPU IS OPERATIONAL"));
+        assertTrue(!controller.serialConsole.toString().trim().contains("ERROR"));
     }
 
     private class BusControllerWithMemory implements BusController {
         final byte[] memory = new byte[0x10000];
         final StringBuilder serialConsole = new StringBuilder();
+        private Z80 cpu;
 
         @Override
         public void mapAndWrite(final int address, final int value) throws IndexOutOfBoundsException {
-            if (address == 0x10003) {
-                serialConsole.append((char) value);
-            } else {
-                memory[address] = (byte) value;
-            }
+            memory[address] = (byte) value;
         }
 
         @Override
         public int mapAndRead(final int address) throws IndexOutOfBoundsException {
-            return memory[address] & 0xFF;
+            if (address == 0x10000) {
+                if (cpu.C() == 2) {
+                    serialConsole.append((char) cpu.E());
+                    System.out.print((char) cpu.E());
+                } else if (cpu.C() == 9) {
+                    for (int i = cpu.DE() & 0xFFFF; memory[i] != '$'; ++i) {
+                        serialConsole.append((char) memory[i]);
+                        System.out.print((char) memory[i]);
+                    }
+                }
+                System.out.flush();
+                return 0;
+            } else {
+                return memory[address] & 0xFF;
+            }
         }
 
         @Override
@@ -99,6 +94,10 @@ public class Z80Test {
 
         @Override
         public void setBusController(@Nullable final BusController controller) {
+        }
+
+        public void setCPU(final Z80 CPU) {
+            this.cpu = CPU;
         }
     }
 }
