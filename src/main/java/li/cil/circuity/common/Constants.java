1416ee63c2e94864fe8ac953b1db8843475d6a41
Florian Nücke <florian@nuecke.de>
2016-08-19 13:34:30 +0200

    Renamed resources to all lowercase.

--- src/main/java/li/cil/circuity/common/Constants.java
+++ src/main/java/li/cil/circuity/common/Constants.java
@@ -1,14 +1,20 @@
 package li.cil.circuity.common;
 
 public final class Constants {
-    public static final String BUS_CONTROLLER_NAME = "busController";
-    public static final String BUS_CABLE_NAME = "busCable";
+    public static final String BUS_CONTROLLER_NAME = "bus_controller";
+    public static final String BUS_CABLE_NAME = "bus_cable";
     public static final String EEPROM_NAME = "eeprom";
-    public static final String EEPROM_READER_NAME = "eepromReader";
-    public static final String PROCESSOR_Z80_NAME = "processorZ80";
-    public static final String RANDOM_ACCESS_MEMORY_NAME = "randomAccessMemory";
-    public static final String REDSTONE_CONTROLLER_NAME = "redstoneController";
-    public static final String SERIAL_CONSOLE_NAME = "serialConsole";
+    public static final String EEPROM_READER_NAME = "eeprom_reader";
+    public static final String PROCESSOR_Z80_NAME = "processor_z80";
+    public static final String RANDOM_ACCESS_MEMORY_NAME = "random_access_memory";
+    public static final String REDSTONE_CONTROLLER_NAME = "redstone_controller";
+    public static final String SERIAL_CONSOLE_NAME = "serial_console";
+
+    // --------------------------------------------------------------------- //
+
+    public static final String I18N_SCAN_ERROR_MULTIPLE_BUS_CONTROLLERS = "bus.error.multiple_controllers";
+    public static final String I18N_SCAN_ERROR_ADDRESSES_OVERLAP = "bus.error.addresses_overlap";
+    public static final String I18N_SCAN_ERROR_SEGMENT_FAILED = "bus.error.segment_failed";
 
     // --------------------------------------------------------------------- //
 
@@ -25,9 +31,16 @@ public final class Constants {
     public static final int EEPROM_ADDRESS = 0xC000; // up to 0xCFFF
     public static final int DISK_DRIVE_ADDRESS = 0xD000;
     public static final int REDSTONE_CONTROLLER_ADDRESS = 0xD000;
-    public static final int SERIAL_CONSOLE_ADDRESS = 0xD000;
+    public static final int SERIAL_CONSOLE_ADDRESS = 0x10002;
 
     public static final int BUS_CONTROLLER_ADDRESS = 0x10000;
+
+    // --------------------------------------------------------------------- //
+
+    /**
+     * The tick rate of Minecraft, just to avoid that magic number everywhere.
+     */
+    public static final int TICKS_PER_SECOND = 20;
 
     private Constants() {
     }
