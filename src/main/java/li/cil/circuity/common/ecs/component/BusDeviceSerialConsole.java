5dfee026c7340918bbaa7cc81fee5572d8dc7c56
Florian Nücke <florian@nuecke.de>
2016-08-30 22:07:57 +0200

    Make serial console use synchronized byte array to store data (and sync it to the client). Also I'm pretty sure the tab insertion logic was inverted.

--- src/main/java/li/cil/circuity/common/ecs/component/BusDeviceSerialConsole.java
+++ src/main/java/li/cil/circuity/common/ecs/component/BusDeviceSerialConsole.java
@@ -10,21 +10,24 @@ import li.cil.circuity.api.bus.device.InterruptList;
 import li.cil.circuity.common.Constants;
 import li.cil.lib.api.ecs.manager.EntityComponentManager;
 import li.cil.lib.api.serialization.Serialize;
+import li.cil.lib.synchronization.value.SynchronizedByteArray;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextComponentTranslation;
 
 import javax.annotation.Nullable;
-import java.util.Arrays;
 
 public final class BusDeviceSerialConsole extends AbstractComponentBusDevice {
-
     public static final int CONS_WIDTH = 40;
     public static final int CONS_HEIGHT = 25;
     public static final int CONS_TAB_STOP = 4;
 
     @Serialize
     private final SerialConsoleImpl device = new SerialConsoleImpl();
+    @Serialize
+    private final SynchronizedByteArray buffer = new SynchronizedByteArray(CONS_WIDTH * CONS_HEIGHT);
+
+    // --------------------------------------------------------------------- //
 
     public BusDeviceSerialConsole(final EntityComponentManager manager, final long entity, final long id) {
         super(manager, entity, id);
@@ -42,17 +45,15 @@ public final class BusDeviceSerialConsole extends AbstractComponentBusDevice {
     public static final DeviceInfo DEVICE_INFO = new DeviceInfo(DeviceType.SERIAL_INTERFACE, Constants.DeviceInfo.SERIAL_CONSOLE_NAME);
 
     public final class SerialConsoleImpl extends AbstractAddressableInterruptSource implements AddressHint {
-        // --------------------------------------------------------------------- //
-        // AbstractAddressableInterruptSource
-
-        @Serialize
-        private char[][] scrBuf = new char[CONS_HEIGHT][CONS_WIDTH];
         @Serialize
         private int scrX = 0; // Range: [0,CONS_WIDTH] (yes, inclusive)
         @Serialize
         private int scrY = 0; // Range: [0,CONS_HEIGHT) (not a typo!)
         @Serialize
         private int scrOffY = 0; // Range: [0,CONS_HEIGHT)
+
+        // --------------------------------------------------------------------- //
+        // AbstractAddressableInterruptSource
 
         @Nullable
         @Override
@@ -76,42 +77,6 @@ public final class BusDeviceSerialConsole extends AbstractComponentBusDevice {
             return new TextComponentTranslation(Constants.I18N.INTERRUPT_SOURCE_KEYBOARD_INPUT);
         }
 
-        private void scrollDown() {
-            Arrays.fill(this.scrBuf[this.scrOffY], (char)0);
-            this.scrOffY++;
-            this.scrOffY %= CONS_HEIGHT;
-            this.scrY--;
-            if(this.scrY < 0) {
-                this.scrY = 0;
-            }
-        }
-
-        private void advanceLine() {
-            // Print line (temporary measure)
-            String outbuf = "";
-            final char[] inbuf = this.scrBuf[(this.scrOffY+this.scrY)%CONS_HEIGHT];
-
-            for(int i = 0; i < this.scrX; i++) {
-                char ch = inbuf[i];
-                if(ch >= (char)0x20) {
-                    outbuf += ch;
-                } else if(ch == '\t') {
-                    outbuf += ' ';
-                }
-            }
-
-            //System.out.print(outbuf + "\n");
-            BusDeviceSerialConsole.this.getWorld().getMinecraftServer().getPlayerList().sendChatMsg(
-                    new TextComponentString("SC: " + outbuf)
-            );
-
-            //
-            this.scrY++;
-            while(this.scrY >= CONS_HEIGHT) {
-                scrollDown();
-            }
-        }
-
         @Override
         public int read(final int address) {
             switch (address) {
@@ -128,27 +93,27 @@ public final class BusDeviceSerialConsole extends AbstractComponentBusDevice {
                 case 0: {
                     final char ch = (char) value;
 
-                    switch(ch) {
+                    switch (ch) {
                         case '\b': // Backspace
                             do {
                                 this.scrX--;
-                            } while(this.scrX >= 0 && this.scrX % CONS_WIDTH == 0
-                                && this.scrBuf[(this.scrOffY+this.scrY)%CONS_HEIGHT][this.scrX] == '\t');
+                            } while (this.scrX >= 0 && this.scrX % CONS_WIDTH != 0
+                                    && get(this.scrX, line()) == '\t');
 
-                            if(this.scrX < 0) {
+                            if (this.scrX < 0) {
                                 this.scrX = 0;
                             }
                             break;
 
                         case '\t': // Tab
                             do {
-                                this.scrBuf[(this.scrOffY+this.scrY)%CONS_HEIGHT][this.scrX] = '\t';
+                                set(this.scrX, line(), '\t');
                                 this.scrX++;
-                                if(this.scrX >= CONS_WIDTH) {
+                                if (this.scrX >= CONS_WIDTH) {
                                     advanceLine();
                                     this.scrX = 0;
                                 }
-                            } while(this.scrX % CONS_TAB_STOP == 0);
+                            } while (this.scrX % CONS_TAB_STOP != 0);
                             break;
 
                         case '\n': // Line feed
@@ -165,11 +130,11 @@ public final class BusDeviceSerialConsole extends AbstractComponentBusDevice {
                             break;
 
                         default:
-                            if(this.scrX >= CONS_WIDTH) {
+                            if (this.scrX >= CONS_WIDTH) {
                                 advanceLine();
                                 this.scrX = 0;
                             }
-                            this.scrBuf[(this.scrOffY+this.scrY)%CONS_HEIGHT][this.scrX] = ch;
+                            set(this.scrX, line(), ch);
                             this.scrX++;
                     }
                     break;
@@ -183,6 +148,60 @@ public final class BusDeviceSerialConsole extends AbstractComponentBusDevice {
         @Override
         public int getSortHint() {
             return Constants.SERIAL_CONSOLE_ADDRESS;
+        }
+
+        // --------------------------------------------------------------------- //
+
+        private int line() {
+            return (this.scrOffY + this.scrY) % CONS_HEIGHT;
+        }
+
+        private char get(final int x, final int y) {
+            return (char) BusDeviceSerialConsole.this.buffer.get(y * CONS_WIDTH + x);
+        }
+
+        private void set(final int x, final int y, final char ch) {
+            BusDeviceSerialConsole.this.buffer.set(y * CONS_WIDTH + x, (byte) ch);
+        }
+
+        private void advanceLine() {
+            debugPrint();
+
+            this.scrY++;
+            while (this.scrY >= CONS_HEIGHT) {
+                scrollDown();
+            }
+        }
+
+        private void scrollDown() {
+            for (int index = scrOffY, end = index + CONS_WIDTH; index < end; index++) {
+                BusDeviceSerialConsole.this.buffer.set(index, (byte) 0);
+            }
+
+            this.scrOffY++;
+            this.scrOffY %= CONS_HEIGHT;
+            this.scrY--;
+            if (this.scrY < 0) {
+                this.scrY = 0;
+            }
+        }
+
+        private void debugPrint() {
+            // Print line (temporary measure)
+            final StringBuilder sb = new StringBuilder();
+            for (int x = 0; x < this.scrX; x++) {
+                final char ch = get(x, line());
+                if (ch >= (char) 0x20) {
+                    sb.append(ch);
+                } else if (ch == '\t') {
+                    sb.append(' ');
+                }
+            }
+
+            //System.out.print(sb.toString() + "\n");
+            BusDeviceSerialConsole.this.getWorld().getMinecraftServer().getPlayerList().sendChatMsg(
+                    new TextComponentString("SC: " + sb.toString())
+            );
         }
     }
 }
