10afb03b1d7f47e2b362da18c97cfeb3847cae91
Florian Nücke <florian@nuecke.de>
2016-08-31 02:25:14 +0200

    Added ScreenRenderer interface.

---
+++ src/main/java/li/cil/circuity/api/bus/device/ScreenRenderer.java
@@ -0,0 +1,20 @@
+package li.cil.circuity.api.bus.device;
+
+import li.cil.circuity.api.bus.BusDevice;
+
+/**
+ * When implemented on a {@link BusDevice}, it may be used by a screen to
+ * render some content via this device.
+ */
+public interface ScreenRenderer extends PersistentIdentifiable {
+    /**
+     * Called when the device should render to a screen.
+     * <p>
+     * The GL state will have been set up such that the device should render
+     * its content on the XZ-plane, in a rectangle of the specified size.
+     *
+     * @param width  the available width for rendering.
+     * @param height the available height for rendering.
+     */
+    void render(final int width, final int height);
+}
