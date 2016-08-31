6baebc130527dd7e61fe92ff57286569d8e68959
Ben Russell <thematrixeatsyou@gmail.com>
2016-08-31 13:41:16 +1200

    add GUI interface to screen

--- src/main/java/li/cil/circuity/common/ecs/component/BusDeviceScreen.java
+++ src/main/java/li/cil/circuity/common/ecs/component/BusDeviceScreen.java
@@ -11,19 +11,34 @@ import li.cil.circuity.api.bus.device.DeviceType;
 import li.cil.circuity.api.bus.device.ScreenRenderer;
 import li.cil.circuity.common.Constants;
 import li.cil.lib.api.SillyBeeAPI;
+import li.cil.lib.api.ecs.component.event.ActivationListener;
 import li.cil.lib.api.ecs.manager.EntityComponentManager;
 import li.cil.lib.api.serialization.Serializable;
 import li.cil.lib.api.serialization.Serialize;
 import li.cil.lib.synchronization.value.SynchronizedUUID;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.world.World;
+import org.lwjgl.opengl.GL11;
 
 import javax.annotation.Nullable;
 import java.util.UUID;
 
-public class BusDeviceScreen extends AbstractComponentBusDevice {
+public class BusDeviceScreen extends AbstractComponentBusDevice implements ActivationListener {
     @Serialize
     private final ScreenImpl device = new ScreenImpl();
     @Serialize
     private final SynchronizedUUID rendererId = new SynchronizedUUID();
+
+    public static final int SCREEN_RES_PIXELS_W = 320;
+    public static final int SCREEN_RES_PIXELS_H = 200;
+
+    private final ScreenGuiImpl screenGui = new ScreenGuiImpl();
 
     // --------------------------------------------------------------------- //
 
@@ -32,7 +47,6 @@ public class BusDeviceScreen extends AbstractComponentBusDevice {
     }
 
     // --------------------------------------------------------------------- //
-
     @Nullable
     public ScreenRenderer getScreenRenderer() {
         final UUID id = rendererId.get();
@@ -60,6 +74,22 @@ public class BusDeviceScreen extends AbstractComponentBusDevice {
         }
     }
 
+    // ActivationListener
+
+    @Override
+    public boolean handleActivated(final EntityPlayer player, final EnumHand hand, @Nullable final ItemStack heldItem, final EnumFacing side, final float hitX, final float hitY, final float hitZ) {
+        if(player.isSneaking()) {
+            return false;
+        }
+
+        final World world = getWorld();
+        if (world.isRemote) {
+            Minecraft.getMinecraft().displayGuiScreen(screenGui);
+            //PlayerUtil.addLocalChatMessage(player, new TextComponentString("There should be a GUI here."));
+        }
+        return true;
+    }
+
     // --------------------------------------------------------------------- //
     // AbstractComponentBusDevice
 
@@ -69,6 +99,96 @@ public class BusDeviceScreen extends AbstractComponentBusDevice {
     }
 
     public static final DeviceInfo DEVICE_INFO = new DeviceInfo(DeviceType.SCREEN, Constants.DeviceInfo.SCREEN_NAME);
+
+    public final class ScreenGuiImpl extends GuiScreen {
+        @Override
+        public void initGui() {
+            super.initGui();
+        }
+
+        @Override
+        public void onGuiClosed() {
+            super.onGuiClosed();
+        }
+
+        @Override
+        public void drawScreen(int mx, int my, float unk1) {
+            super.drawScreen(mx, my, unk1);
+
+            // SETTINGS
+            final int windowGapSize = 5;
+            final int screenBorderSize = 10;
+            final int innerBorderSize = 5;
+
+            // Get GUI dimensions and centre
+            int sw = this.width;
+            int sh = this.height;
+            int scx = sw/2;
+            int scy = sh/2;
+
+            // Get inner size
+            int innerW = sw-windowGapSize-screenBorderSize-innerBorderSize;
+            int innerH = sh-windowGapSize-screenBorderSize-innerBorderSize;
+
+            // Shrink to fit aspect ratio
+            if(innerW*SCREEN_RES_PIXELS_H > innerH*SCREEN_RES_PIXELS_W) {
+                innerW = (innerH*SCREEN_RES_PIXELS_W)/SCREEN_RES_PIXELS_H;
+            } else {
+                innerH = (innerW*SCREEN_RES_PIXELS_H)/SCREEN_RES_PIXELS_W;
+            }
+
+            // Get screen rect size
+            int innerBorderW = innerW + innerBorderSize;
+            int innerBorderH = innerH + innerBorderSize;
+
+            // Get outer size
+            int outerW = innerBorderW + screenBorderSize;
+            int outerH = innerBorderH + screenBorderSize;
+
+            // Set up GL state
+            GlStateManager.pushAttrib();
+            GlStateManager.disableTexture2D();
+            GlStateManager.blendFunc(GL11.GL_ONE, GL11.GL_ZERO);
+
+            // Start drawing frame
+            GlStateManager.glBegin(GL11.GL_QUADS);
+
+            // Outer rectangle
+            GlStateManager.color(0.8f, 0.7f, 0.6f);
+            GlStateManager.glVertex3f( scx-outerW/2f, scy-outerH/2f, 0f);
+            GlStateManager.glVertex3f( scx-outerW/2f, scy+outerH/2f, 0f);
+            GlStateManager.glVertex3f( scx+outerW/2f, scy+outerH/2f, 0f);
+            GlStateManager.glVertex3f( scx+outerW/2f, scy-outerH/2f, 0f);
+
+            // Inner rectangle (actual screen)
+            GlStateManager.color(0.0f, 0.0f, 0.0f);
+            GlStateManager.glVertex3f( scx-innerBorderW/2f, scy-innerBorderH/2f, 0f);
+            GlStateManager.glVertex3f( scx-innerBorderW/2f, scy+innerBorderH/2f, 0f);
+            GlStateManager.glVertex3f( scx+innerBorderW/2f, scy+innerBorderH/2f, 0f);
+            GlStateManager.glVertex3f( scx+innerBorderW/2f, scy-innerBorderH/2f, 0f);
+
+            // Finish drawing frame
+            GlStateManager.glEnd();
+
+            // Clean up GL state
+            GlStateManager.enableTexture2D();
+            GlStateManager.popAttrib();
+
+            // Draw screen
+            ScreenRenderer renderer = BusDeviceScreen.this.getScreenRenderer();
+            if(renderer != null) {
+                GlStateManager.translate( (scx-innerW/2f),  (scy-innerH/2f), 0f);
+                renderer.render(innerW, innerH);
+                GlStateManager.translate(-(scx-innerW/2f), -(scy-innerH/2f), 0f);
+            }
+        }
+
+        @Override
+        public boolean doesGuiPauseGame() {
+            return false;
+        }
+    }
+
 
     @Serializable
     public final class ScreenImpl extends AbstractAddressable implements AddressHint, BusChangeListener {
