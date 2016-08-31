48d8b9f9c5dc812e7a662338ee797a404d09fadc
Florian Nücke <florian@nuecke.de>
2016-08-31 02:36:41 +0200

    Mark dirty when changing serial console buffer.

--- src/main/java/li/cil/circuity/common/ecs/component/BusDeviceSerialConsole.java
+++ src/main/java/li/cil/circuity/common/ecs/component/BusDeviceSerialConsole.java
@@ -198,6 +198,9 @@ public final class BusDeviceSerialConsole extends AbstractComponentBusDevice imp
                             break;
                         }
                     }
+
+                    BusDeviceSerialConsole.this.markChanged();
+
                     break;
                 }
             }
