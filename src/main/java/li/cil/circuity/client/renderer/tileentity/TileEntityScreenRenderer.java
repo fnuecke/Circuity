36de613b400abdafc3e5d071a19542b61a857eb7
Florian Nücke <florian@nuecke.de>
2016-08-31 02:26:10 +0200

    Make screen use new APIs, render in TESR (very very WIP).

--- src/main/java/li/cil/circuity/client/renderer/tileentity/TileEntityScreenRenderer.java
+++ src/main/java/li/cil/circuity/client/renderer/tileentity/TileEntityScreenRenderer.java
@@ -1,11 +1,38 @@
 package li.cil.circuity.client.renderer.tileentity;
 
+import li.cil.circuity.api.bus.device.ScreenRenderer;
+import li.cil.circuity.common.ecs.component.BusDeviceScreen;
 import li.cil.circuity.common.tileentity.TileEntityScreen;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+
+import java.util.Optional;
 
 public class TileEntityScreenRenderer extends TileEntitySpecialRenderer<TileEntityScreen> {
     @Override
     public void renderTileEntityAt(final TileEntityScreen te, final double x, final double y, final double z, final float partialTicks, final int destroyStage) {
-        super.renderTileEntityAt(te, x, y, z, partialTicks, destroyStage);
+        final Optional<BusDeviceScreen> maybeScreen = te.getComponent(BusDeviceScreen.class);
+        if (maybeScreen.isPresent()) {
+            final BusDeviceScreen screen = maybeScreen.get();
+            final ScreenRenderer renderer = screen.getScreenRenderer();
+            if (renderer != null) {
+                GlStateManager.pushMatrix();
+                this.func_190053_a(true);
+                GlStateManager.disableCull();
+
+                GlStateManager.translate(x, y, z);
+                GlStateManager.translate(0, 2, 0.5f);
+                GlStateManager.scale(1, -1, 1);
+                GlStateManager.scale(1/256f, 1/256f, 1/256f);
+
+                renderer.render(256, 256);
+
+                super.renderTileEntityAt(te, x, y, z, partialTicks, destroyStage);
+
+                GlStateManager.enableCull();
+                this.func_190053_a(false);
+                GlStateManager.popMatrix();
+            }
+        }
     }
 }
--- src/main/java/li/cil/circuity/common/ecs/component/BusDeviceScreen.java
+++ src/main/java/li/cil/circuity/common/ecs/component/BusDeviceScreen.java
@@ -1,25 +1,82 @@
 package li.cil.circuity.common.ecs.component;
 
+import li.cil.circuity.ModCircuity;
 import li.cil.circuity.api.bus.BusDevice;
 import li.cil.circuity.api.bus.device.AbstractBusDevice;
+import li.cil.circuity.api.bus.device.BusChangeListener;
+import li.cil.circuity.api.bus.device.ScreenRenderer;
+import li.cil.lib.api.SillyBeeAPI;
 import li.cil.lib.api.ecs.manager.EntityComponentManager;
 import li.cil.lib.api.serialization.Serializable;
 import li.cil.lib.api.serialization.Serialize;
+import li.cil.lib.synchronization.value.SynchronizedUUID;
+
+import javax.annotation.Nullable;
+import java.util.UUID;
 
 public class BusDeviceScreen extends AbstractComponentBusDevice {
     @Serialize
     private final ScreenImpl device = new ScreenImpl();
+    @Serialize
+    private final SynchronizedUUID rendererId = new SynchronizedUUID();
+
+    // --------------------------------------------------------------------- //
 
     public BusDeviceScreen(final EntityComponentManager manager, final long entity, final long id) {
         super(manager, entity, id);
     }
+
+    // --------------------------------------------------------------------- //
+
+    @Nullable
+    public ScreenRenderer getScreenRenderer() {
+        final UUID id = rendererId.get();
+        if (id != null) {
+            final Object renderer = SillyBeeAPI.globalObjects.get(getWorld(), id);
+            if (renderer instanceof ScreenRenderer) {
+                return (ScreenRenderer) renderer;
+            } else if (renderer != null) {
+                ModCircuity.getLogger().warn("Got an incompatible type retrieving ScreenRenderer. UUID collision?");
+            }
+        }
+        return null;
+    }
+
+    // --------------------------------------------------------------------- //
+    // AbstractComponent
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        final UUID id = rendererId.get();
+        if (id != null) {
+            SillyBeeAPI.globalObjects.remove(getWorld(), id);
+        }
+    }
+
+    // --------------------------------------------------------------------- //
+    // AbstractComponentBusDevice
 
     @Override
     public BusDevice getDevice() {
         return device;
     }
 
+    // --------------------------------------------------------------------- //
+
     @Serializable
-    public final class ScreenImpl extends AbstractBusDevice {
+    public final class ScreenImpl extends AbstractBusDevice implements BusChangeListener {
+        @Override
+        public void handleBusChanged() {
+            // TODO Build list of candidates, allow user to select current one.
+            for (final BusDevice device : controller.getDevices()) {
+                if (device instanceof ScreenRenderer) {
+                    final ScreenRenderer renderer = (ScreenRenderer) device;
+                    BusDeviceScreen.this.rendererId.set(renderer.getPersistentId());
+                    return;
+                }
+            }
+        }
     }
 }
