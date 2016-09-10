3f3fc13e6e91227f13f143d8e423e476b7a5d4e6
Florian Nücke <florian@nuecke.de>
2016-09-10 22:44:06 +0200

    Oh right.

---
+++ src/main/java/li/cil/circuity/api/bus/controller/package-info.java
@@ -0,0 +1,7 @@
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+package li.cil.circuity.api.bus.controller;
+
+import mcp.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
