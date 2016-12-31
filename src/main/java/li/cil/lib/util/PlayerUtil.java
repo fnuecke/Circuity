b7ab50a5397f6ea11930a2103372560f094668f7
Florian Nücke <florian@nuecke.de>
2016-12-31 16:14:29 +0100

    Utility method for getting held stack of specific type.

--- src/main/java/li/cil/lib/util/PlayerUtil.java
+++ src/main/java/li/cil/lib/util/PlayerUtil.java
@@ -2,6 +2,8 @@ package li.cil.lib.util;
 
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -12,6 +14,35 @@ public final class PlayerUtil {
         if (player instanceof EntityPlayerSP) {
             player.sendMessage(message);
         }
+    }
+
+    public static ItemStack getHeldItemOfType(final EntityPlayer player, final Item filter) {
+        return getHeldItemOfType(player, new ItemStack(filter));
+    }
+
+    public static ItemStack getHeldItemOfType(final EntityPlayer player, final ItemStack filter) {
+        return getHeldItemOfType(player, filter, ItemStack::isItemEqual);
+    }
+
+    public static ItemStack getHeldItemOfType(final EntityPlayer player, final ItemStack filter, final StackComparator comparator) {
+        final ItemStack mainStack = player.getHeldItemMainhand();
+        if (comparator.compare(mainStack, filter)) {
+            return mainStack;
+        }
+
+        final ItemStack offStack = player.getHeldItemOffhand();
+        if (comparator.compare(offStack, filter)) {
+            return offStack;
+        }
+
+        return ItemStack.EMPTY;
+    }
+
+    // --------------------------------------------------------------------- //
+
+    @FunctionalInterface
+    public interface StackComparator {
+        boolean compare(final ItemStack stackA, final ItemStack stackB);
     }
 
     // --------------------------------------------------------------------- //
